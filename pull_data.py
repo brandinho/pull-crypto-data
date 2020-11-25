690a867cabe4a0edcc56eabcd36ab983af41fe6e
Brandon Da Silva <brandasilva9@gmail.com>
2020-11-25 15:06:07 -0500

    Initial Commit

---
+++ pull_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 14:48:12 2020
+
+@author: brandinho
+"""
+
+import cryptowatch as cw
+import pandas as pd
+import numpy as np
+
